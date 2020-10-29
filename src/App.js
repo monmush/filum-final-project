d7b7be2c6a5c1830b18f4c4ffe6605ebe786ea45
Minh Pham <omiph001@gmail.com>
2020-10-29 11:13:10 +0700

    define public and private route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Login from './pages/Login'
-import Home from './pages/Home'
+import Categories from './pages/Categories'
 import Register from './pages/Register'
+import Items from './pages/Items'
 import { getToken } from './libs/request'
-
 function App() {
   return (
     <Router>
       <Switch>
-        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute exact path="/" component={Categories} />
+        <PrivateRoute exact path="/categories/:category_id" component={Items} />
         <AuthRoute path="/login" component={Login} />
         <AuthRoute path="/register" component={Register} />
         <Redirect to="/" />
